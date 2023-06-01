dda25a9cd98f2160797515a9e9115ae62d00fdba
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-06-01 14:30:22 +0200

    add Snack9

--- src/snacks/Snack9.java
+++ src/snacks/Snack9.java
@@ -1,4 +1,23 @@
 package snacks;
 
 public class Snack9 {
+    public static void main(String[] args) {
+      /*  Calcola la somma e la media dei primi 10 numeri.*/
+        int number;
+        int sum = 0;
+        double media = 0;
+
+
+        for (int i = 1; i <= 10; i++) {
+             number = i;
+             sum += number;
+
+             media = (double) sum /10;
+
+
+
+        }
+        System.out.println("La somma dei numeri da 1 a 10 è: "+ sum);
+        System.out.println("La media dei numeri è: " + media);
+    }
 }
